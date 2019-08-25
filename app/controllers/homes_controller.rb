436e33e3bc246466ee104ccf37fc9a068c0cdf46
Tomoya-N <t.nakamurax@gmail.com>
2019-08-25 15:14:49 +0900

    add about

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,4 +2,10 @@ class HomesController < ApplicationController
   def index
     @message = "Hello World"
   end
+
+  def about
+    @about = "Hello Worldと出力するアプリです。"
+    @name = "中村"
+    @email = "hogehogehoge@hoge.com"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get 'homes', to: 'homes#index'
+  get 'homes/about', to: 'homes#about'
 end
